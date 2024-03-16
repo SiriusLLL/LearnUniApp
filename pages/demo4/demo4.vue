186c786aac8b347dfcc3fc9540ec6f937d5f4b7e
Lh <lihao1017@qq.com>
2024-03-16 11:48:13 +0800

    add(demo4):v-on

---
+++ pages/demo4/demo4.vue
@@ -0,0 +1,33 @@
+<template>
+	<view class="box" @click="onClick">
+		{{num}}
+	</view>
+
+	<switch @change="onChange"></switch>
+	<button type="primary" :disabled="isDisabled"> 普通按钮</button>
+</template>
+
+<script setup>
+	import {
+		ref
+	} from "vue";
+	const num = ref(1);
+	const isDisabled = ref(false);
+
+	function onClick() {
+		num.value++;
+	}
+
+	function onChange(e) {
+		isDisabled.value = e.detail.value;
+		// console.log(e.detail.value);
+	}
+</script>
+
+<style lang="scss">
+	.box {
+		width: 200px;
+		height: 200px;
+		background: orange;
+	}
+</style>
