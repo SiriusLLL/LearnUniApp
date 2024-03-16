1d91a51b92feddebf66223221e67756ab380a3c4
Lh <lihao1017@qq.com>
2024-03-16 14:38:23 +0800

    add(demo5):v-for

---
+++ pages/demo5/demo5.vue
@@ -0,0 +1,28 @@
+<template>
+	<view class="out">
+		<view class="box" v-for="(item,index) in 10">box模块{{index + 1}}</view>
+		<view v-for="item in nba">
+			球星：{{item.name}} - 号码：{{item.num}}
+		</view>
+	</view>
+</template>
+
+<script setup>
+	import {
+		ref
+	} from "vue";
+	const nba = ref([{
+		name: "乔丹",
+		num: 23
+	}, {
+		name: "詹姆斯",
+		num: 6
+	}, {
+		name: "科比",
+		num: 24
+	}])
+</script>
+
+<style lang="scss" scoped>
+
+</style>
