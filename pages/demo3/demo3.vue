6ca187445abaf06934f8703cac64ed17418d28d9
Lh <lihao1017@qq.com>
2024-03-16 11:26:47 +0800

    fix(demo3):v-bind

--- pages/demo3/demo3.vue
+++ pages/demo3/demo3.vue
@@ -3,6 +3,11 @@
 		<image :src="picUrl" mode=""></image>
 		<button type="primary" :loading="false" :disabled="true">按钮</button>
 	</view>
+	<view class="box" :class="{active:isActive}">v-bind指令</view>
+
+	<view class="box" :class="isActive?'active' : ''"></view>
+
+	<view class="box" :style="{width:'300px',height:260+'px',fontSize:size+'px'}">内联样式</view>
 </template>
 
 <script setup>
@@ -10,6 +15,8 @@
 		ref
 	} from "vue";
 
+	const isActive = ref(false);
+	const size = ref(30)
 	const arrs = ref(["../../static/hc.png", "../../static/haha.png", "../../static/wxr.png", "../../static/xixi.png"]);
 	const picUrl = ref("../../static/hc.png");
 
@@ -17,9 +24,20 @@
 	setInterval(() => {
 		i++;
 		picUrl.value = arrs.value[i % 4];
-	}, 1000)
+		isActive.value = !isActive.value;
+		// size.value += i;
+	}, 1000);
 </script>
 
-<style>
+<style lang="scss">
+	.box {
+		width: 200px;
+		height: 200px;
+		background: orange;
+		font-size: 20px;
+	}
 
+	.active {
+		background: #fff;
+	}
 </style>
