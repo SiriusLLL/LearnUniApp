376895a3b3fafb86159b9e61006a796b95bfbb52
Lh <lihao1017@qq.com>
2024-03-16 14:46:20 +0800

    fix(demo5):v-for

--- pages/demo5/demo5.vue
+++ pages/demo5/demo5.vue
@@ -1,7 +1,7 @@
 <template>
 	<view class="out">
-		<view class="box" v-for="(item,index) in 10">box模块{{index + 1}}</view>
-		<view v-for="item in nba">
+		<view class="box" v-for="(item,index) in 10" :key="index">box模块{{index + 1}}</view>
+		<view v-for="item in nba" :key="item.id">
 			球星：{{item.name}} - 号码：{{item.num}}
 		</view>
 	</view>
@@ -12,12 +12,15 @@
 		ref
 	} from "vue";
 	const nba = ref([{
+		id: 1,
 		name: "乔丹",
 		num: 23
 	}, {
+		id: 2,
 		name: "詹姆斯",
 		num: 6
 	}, {
+		id: 3,
 		name: "科比",
 		num: 24
 	}])
