ecec5f4719e495d93215bd201f2610cd96b630d9
Lh <lihao1017@qq.com>
2024-03-16 11:00:13 +0800

    add(demo3):v-bind

---
+++ pages/demo3/demo3.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		<image :src="picUrl" mode=""></image>
+		<button type="primary" :loading="false" :disabled="true">按钮</button>
+	</view>
+</template>
+
+<script setup>
+	import {
+		ref
+	} from "vue";
+
+	const arrs = ref(["../../static/hc.png", "../../static/haha.png", "../../static/wxr.png", "../../static/xixi.png"]);
+	const picUrl = ref("../../static/hc.png");
+
+	let i = 0;
+	setInterval(() => {
+		i++;
+		picUrl.value = arrs.value[i % 4];
+	}, 1000)
+</script>
+
+<style>
+
+</style>
